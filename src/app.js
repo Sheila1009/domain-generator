8796a85f66c9a01f451ddae25376ee06ad350cd1
Sheila1009 <122435673+Sheila1009@users.noreply.github.com>
2023-05-08 18:50:45 +0000

    entrega

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".es", ".com "];
+
+  function generatorDomainNames(pronoun, adj, noun, ext) {
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let o = 0; o < adj.length; o++) {
+        for (let u = 0; u < noun.length; u++) {
+          for (let a = 0; a < ext.length; a++) {
+            let domainNames = pronoun[i] + adj[o] + noun[u] + ext[a];
+            console.log(domainNames);
+          }
+        }
+      }
+    }
+  }
+
+  generatorDomainNames(pronoun, adj, noun, ext);
+  console.log(generatorDomainNames(pronoun, adj, noun, ext));
 };
